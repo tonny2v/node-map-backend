de24e526627bb1392da4734014263ef0da06c850
tonny <tonny.achilles@gmail.com>
2019-01-09 12:18:17 +0800

    refactor pgvt.js

--- routes/pgvt.js
+++ routes/pgvt.js
@@ -3,42 +3,24 @@ const mapnik = require('mapnik');
 const Promise = require('promise');
 const SphericalMercator = require('sphericalmercator');
 const Router = require('express-promise-router');
+
 const router = new Router();
 
 module.exports = router;
 
-let mercator = new SphericalMercator({
-  size: 256 //tile size
+const mercator = new SphericalMercator({
+  size: 256, // tile size
 });
 
 mapnik.register_default_input_plugins();
 
-router.get('/:layerName/:x/:y/:z', function(req, res) {
-  let options = {
-    x: parseInt(req.params.x),
-    y: parseInt(req.params.y),
-    z: parseInt(req.params.z),
-    layerName: req.params.layerName
-  };
-    // console.log(options);
-  makeVectorTile(options).then(function(vectorTile) {
-    zlib.deflate(vectorTile, function(err, data) {
-      if (err) return res.status(500).send(err.message);
-      res.setHeader('Content-Encoding', 'deflate');
-      res.setHeader('Content-Type', 'application/x-protobuf');
-      // console.log(data.byteLength);
-      res.send(data);
-    });
-  });
-});
 
 function makeVectorTile(options) {
-
-  let extent = mercator.bbox(options.x, options.y, options.z, false, '3857');
-  let map = new mapnik.Map(256, 256, '+init=epsg:3857');
+  const extent = mercator.bbox(options.x, options.y, options.z, false, '3857');
+  const map = new mapnik.Map(256, 256, '+init=epsg:3857');
   map.extent = extent;
 
-  let layer = new mapnik.Layer(options.layerName);
+  const layer = new mapnik.Layer(options.layerName);
   layer.datasource = new mapnik.Datasource({
     // host, user, ... are optional, since they have been defined in the start script
     host: 'localhost',
@@ -49,15 +31,34 @@ function makeVectorTile(options) {
     port: 5432,
     password: 'password',
     geometry_field: 'geom',
-    srid: 4326
+    srid: 4326,
   });
   layer.styles = ['default'];
   map.add_layer(layer);
-  return new Promise(function (resolve, reject) {
-    let vtile = new mapnik.VectorTile(parseInt(options.z), parseInt(options.x), parseInt(options.y));
-    map.render(vtile, function (err, vtile) {
+  return new Promise(((resolve, reject) => {
+    const tile = new mapnik.VectorTile(+options.z, +options.x, +options.y);
+    map.render(tile, (err, data) => {
       if (err) return reject(err);
-      resolve(vtile.getData());
+      resolve(data.getData());
+    });
+  }));
+}
+
+router.get('/:layerName/:x/:y/:z', (req, res) => {
+  const options = {
+    x: +req.params.x,
+    y: +req.params.y,
+    z: +req.params.z,
+    layerName: req.params.layerName,
+  };
+    // console.log(options);
+  makeVectorTile(options).then((vectorTile) => {
+    zlib.deflate(vectorTile, (err, data) => {
+      if (err) return res.status(500).send(err.message);
+      res.setHeader('Content-Encoding', 'deflate');
+      res.setHeader('Content-Type', 'application/x-protobuf');
+      // console.log(data.byteLength);
+      res.send(data);
     });
   });
-}
+});
